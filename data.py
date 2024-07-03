a3da8290e4403581235766b9897ebee1d2e67302
Shugalyans <shugalyan@mail.ru>
2024-07-03 22:14:23 +0300

    2st

--- data.py
+++ data.py
@@ -10,3 +10,16 @@ orderEndpoints = ['orders?courierId=335921',
           'orders?limit=10&page=0',
          'orders?limit=10&page=0&nearestStation=["110"]'
          ]
+
+color = None
+order = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [color]
+}
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -2,7 +2,7 @@ import allure
 import requests
 import json
 import pytest
-from Sprint_7.data import colors, URL
+from data import colors, URL, order
 
 
 
@@ -12,18 +12,7 @@ class TestCreateOrder:
         'Проверяем, что заказ можно создать с любым цветом или без него'
     )
     @pytest.mark.parametrize('color', colors)
-    def test_set_color(self, color):
-        order =  {
-                  "firstName": "Naruto",
-                  "lastName": "Uchiha",
-                  "address": "Konoha, 142 apt.",
-                  "metroStation": 4,
-                  "phone": "+7 800 355 35 35",
-                  "rentTime": 5,
-                  "deliveryDate": "2020-06-06",
-                  "comment": "Saske, come back to Konoha",
-                  "color": [color]
-        }
+    def test_set_color(self, order, color):
         json_string = json.dumps(order)
         response = requests.post(f'{URL}/orders', json_string)
         assert response.status_code == 201 and 'track' in response.text
